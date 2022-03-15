6eafecfff851f57dc96f864746149269e4e189fa
mosheabecasis <mosh14abou@yahoo.com>
2022-03-15 20:00:52 +0200

    host2

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://github.com/mosheabecasis/Dosik-Tube');
+const socket = io('https://dosik-video.netlify.app');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
